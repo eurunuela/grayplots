18296cfc65154895af7b103be896acf29da26960
eurunuela <enekouru@gmail.com>
2020-02-19 12:33:21 +0100

    Added more options to parser

--- grayplots/cli/run.py
+++ grayplots/cli/run.py
@@ -14,13 +14,20 @@ def _get_parser():
                           type=str,
                           help=('Input data.'),
                           required=True)
-    required.add_argument('-m',
+    required.add_argument('-mask',
                           dest='mask',
                           nargs='+',
                           metavar='FILE',
                           type=str,
                           help=('Mask of input data.'),
                           required=True)
+    optional.add_argument('-mot',
+                          dest='mot',
+                          nargs='+',
+                          metavar='MOT',
+                          type=str,
+                          help='Motion parameters of each input data (default = None).',
+                          default='None')
     optional.add_argument('-dir',
                           dest='outdir',
                           nargs=1,
@@ -28,6 +35,47 @@ def _get_parser():
                           type=str,
                           help='Output directory name (default = results).',
                           default='results')
+    optional.add_argument('-polort',
+                          dest='polort',
+                          nargs=1,
+                          metavar='POLORT',
+                          type=int,
+                          help='Polort value (int) for 3dGrayplot (default = 0).',
+                          default=0)
+    optional.add_argument('-fwhm',
+                          dest='fwhm',
+                          nargs=1,
+                          metavar='FWHM',
+                          type=float,
+                          help='FWHM value for 3dGrayplot (default = 0.0).',
+                          default=0.0)
+    optional.add_argument('-range',
+                          dest='range',
+                          nargs=1,
+                          metavar='RANGE',
+                          type=int,
+                          help='Range value for 3dGrayplot (default = 3).',
+                          default=3)
+    optional.add_argument('-percent',
+                          dest='percent',
+                          action='store_true',
+                          help='Percent option for 3dGrayplot (default = True).',
+                          default=True)
+    optional.add_argument('-ordering',
+                          dest='ordering',
+                          action='store',
+                          choices=['pvorder', 'LJorder', 'peelorder', 'ijkorder'],
+                          help=('Option for the ordering of voxels in the grayplot, '
+                                'in the vertical direction (default = pvorder).'),
+                          default='pvorder')
+    optional.add_argument('-dim',
+                          dest='dim',
+                          nargs=2,
+                          metavar='DIM',
+                          type=int,
+                          help='Output size of image in pixels for a dpi of 300 '
+                               '(default = 1024, 512).',
+                          default=[1024, 512])
     optional.add_argument('-v', '--version', action='version',
                           version=('%(prog)s ' + __version__))
     parser._action_groups.append(optional)
--- grayplots/grayplots.py
+++ grayplots/grayplots.py
@@ -3,9 +3,43 @@ import numpy as np
 from grayplots.cli.run import _get_parser
 
 
-def grayplots(data, mask, dir):
+def grayplots(data, mask, mot, outdir, polort, fwhm, range, percent, ordering, dim):
     
     n_datasets = len(data)
+
+    print(data)
+
+    if not os.path.isdir(outdir):
+        os.mkdir(outdir)
+        
+    for data_idx in range(n_datasets):
+        print(f'{data[data_idx]}')
+
+#     # grayplot command
+# 3dGrayplot -overwrite -mask mask4grayplot.FUNC.nii.gz -polort ${POLORTORDER} -fwhm ${FWHM} -range 3 -percent -prefix ${SUBJ}.Grayplot.jpg pb04.${SUBJ}.volreg.nii.gz
+# ​
+# echo -e "\e[32m ++ INFO: Computing demean and derivative of realignment parameters ...\e[39m"
+# # compute de-meaned motion parameters (for use in regression)
+# 1d_tool.py -overwrite -infile ${SUBJ}_Motion.1D -set_nruns 1 -demean -write ${SUBJ}_Motion_demean.1D
+# # compute motion parameter derivatives (for use in regression)
+# 1d_tool.py -overwrite -infile ${SUBJ}_Motion.1D -set_nruns 1 -derivative -demean -write ${SUBJ}_Motion_deriv.1D
+# ​
+# # Compute Framewise Displacement based on Euclidean Norm (as default in AFNI)
+# echo -e "\e[32m ++ INFO: Computing Framewise Displacement based on Euclidean Norm (as default in AFNI) ...\e[39m"
+# 1d_tool.py -overwrite -infile ${SUBJ}_Motion.1D -derivative -collapse_cols euclidean_norm -write ${SUBJ}_Motion_enorm.1D
+# # Compute Framewise Displacement (as defined by Power)
+# echo -e "\e[32m ++ INFO: Computing Framewise Displacement (as defined by Power) ...\e[39m"
+# 1deval -overwrite -a ${SUBJ}_Motion_deriv.1D[0] -b ${SUBJ}_Motion_deriv.1D[1] \
+#   -c ${SUBJ}_Motion_deriv.1D[2] -d ${SUBJ}_Motion_deriv.1D[3] \
+#   -e ${SUBJ}_Motion_deriv.1D[4] -f ${SUBJ}_Motion_deriv.1D[5] \
+#   -expr 'abs(a)+abs(b)+abs(c)+abs(d)+abs(e)+abs(f)' > ${SUBJ}_Motion_FD.1D
+# ​
+# # Compute RMS of motion derivative parameters
+# echo -e "\e[32m ++ INFO: Computing Framewise Displacement based on RMS of realignment  derivative parameters ...\e[39m"
+# 1deval -overwrite -a ${SUBJ}_Motion_deriv.1D[0] -b ${SUBJ}_Motion_deriv.1D[1] \
+#   -c ${SUBJ}_Motion_deriv.1D[2] -d ${SUBJ}_Motion_deriv.1D[3] \
+#   -e ${SUBJ}_Motion_deriv.1D[4] -f ${SUBJ}_Motion_deriv.1D[5] \
+#   -expr 'sqrt( (a*a + b*b +c*c + d*d + e*e + f*f)/6 )' > ${SUBJ}_Motion_RMS.1D
 
 
 def _main(argv=None):
